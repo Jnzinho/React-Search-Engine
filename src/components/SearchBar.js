f09a8c7c2b28dbe918c0c529a670a82eaed7d502
Jnzinho <joaovitorjn@unochapeco.edu.br>
2023-02-16 18:05:08 -0300

    Added version 1.0

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./SearchBar.css";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <label>Enter Search Term</label>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
